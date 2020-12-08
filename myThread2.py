8032bed815d108659306f06453e42962181ee6a3
Wales-Wyf <wangyifa17@mails.tsinghua.edu.cn>
2020-12-08 11:07:55 +0800

    copyright

--- myThread2.py
+++ myThread2.py
@@ -1,4 +1,8 @@
+# Copyright 2020 Center for Intelligent and Networked Systems.
+# This program is distributed under the Apache license 2.0.
+# Supported by National Key Research and Development Project of China (No. 2017YFC0704100 entitled New generation intelligent building platform techniques) and the National Natural Science Foundation of China (No. 61425027), the 111 International Collaboration Program of China under Grant BP2018006.
 # encoding: utf-8
+
 from PyQt5 import QtCore,QtWidgets
 import os
 import json
