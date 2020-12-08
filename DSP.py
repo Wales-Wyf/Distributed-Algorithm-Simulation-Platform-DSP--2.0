0c6e8cf5a8998f315a84ab6fb31a10c6452d3870
Wales-Wyf <wangyifa17@mails.tsinghua.edu.cn>
2020-12-08 10:18:37 +0800

    authors

--- DSP.py
+++ DSP.py
@@ -1,3 +1,7 @@
+# Copyright 2020 Center for Intelligent and Networked Systems.
+# This program is distributed under the Apache license 2.0.
+# Supported by National Key Research and Development Project of China (No. 2017YFC0704100 entitled New generation intelligent building platform techniques) and the National Natural Science Foundation of China (No. 61425027), the 111 International Collaboration Program of China under Grant BP2018006.
+
 import sys
 from PyQt5.QtWidgets import QApplication , QMainWindow
 from IoTGUI import *
