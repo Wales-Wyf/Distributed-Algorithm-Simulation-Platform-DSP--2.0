48772460087f99dd21dd21fdefedb2bffed5be4f
Wales-Wyf <wangyifa17@mails.tsinghua.edu.cn>
2020-12-08 11:51:16 +0800

    copyright

--- IoT/server.py
+++ IoT/server.py
@@ -1,7 +1,6 @@
-# Copyright 2020 Center for Intelligent and Networked Systems.
+# Copyright 2020 Center for Intelligent and Networked Systems, Department of Automation, Tsinghua University, Beijing, China.
 # This program is distributed under the Apache license 2.0.
 # Supported by National Key Research and Development Project of China (No. 2017YFC0704100 entitled New generation intelligent building platform techniques) and the National Natural Science Foundation of China (No. 61425027), the 111 International Collaboration Program of China under Grant BP2018006.
-# 
 import json
 import threading
 import socket
